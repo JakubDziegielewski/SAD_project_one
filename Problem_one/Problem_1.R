fb0b8ce48785012a28de778f15dbfa78d536e241
JakubDziegielewski <kubadziegiel1@gmail.com>
2024-05-01 13:32:02 +0200

    Modified grouping and splitting; imporved plotting

--- Problem_one/Problem_1.R
+++ Problem_one/Problem_1.R
@@ -2,35 +2,50 @@ library("tidyverse")
 
 data <- read.csv2("food_statistics.csv")
 data$opis_okres <- as.Date(paste(substring(data$opis_okres, 1, 4), substring(data$opis_okres, 7, 8), "01", sep = "-"))
-important_columns <- data[c("nazwa_pozycja_2", "opis_okres", "wartosc", "jednostka_terytorialna")]
-masovian_voievoidship <- important_columns[important_columns$jednostka_terytorialna == "MAZOWIECKIE",]
-poland <- important_columns[important_columns$jednostka_terytorialna == "POLSKA",]
-split_localization <- function(localization){
-    return (localization |> split(localization$nazwa_pozycja_2))
-}
+months <- data |> select(opis_okres) |> distinct()
+important_columns <- data |> select(nazwa_pozycja_2, opis_okres, wartosc, jednostka_terytorialna)
+masovian_voievoidship <- important_columns |> filter(jednostka_terytorialna == "MAZOWIECKIE")
+poland <- important_columns |> filter(jednostka_terytorialna == "POLSKA")
+
 group_localization <- function(localization){
     return (localization |> group_by(nazwa_pozycja_2))
 }
-split_poland <- split_localization(poland)
-split_masovian <- split_localization(masovian_voievoidship)
+
 groups_pl <- group_localization(poland)
 groups_masovian <- group_localization(masovian_voievoidship)
-
-plot_prices <- function(price_list){
-    for(product in price_list){
-        plot(product$opis_okres, product$wartosc, main = paste("Cena:", product$nazwa_pozycja_2[1], "; Lokalizacja:", product$jednostka_terytorialna[1]), xlab = "Okres", ylab = "Cena")
-    }
-}
-
 
 calculate_inflation <- function(prices){
     return (100 * (tail(prices, n = 1) - prices[1]) / prices[1])
 }
+
+inflation_masovian <- calculate_inflation(groups_masovian$wartosc)
+inflation_pl <- calculate_inflation(groups_pl$wartosc)
+
+
 print_inflation <- function(df){
     df |> group_by(nazwa_pozycja_2) |> summarize(inflation = calculate_inflation(wartosc))
 }
+
+
+
 print_inflation(groups_masovian)
 print_inflation(groups_pl)
-plot_prices(split_masovian)
-plot_prices(split_poland)
 
+
+
+create_plot_for_every_group <- function(group){
+    list_of_products <- split(group, group$nazwa_pozycja_2)
+    for(product in list_of_products){
+        print(product  |> ggplot(aes(x = opis_okres, y = wartosc)) + 
+                  geom_point() + geom_line() +
+                  scale_y_continuous(labels = scales::number_format(accuracy = 0.1)) +
+                  labs(title = "Cena produktu w ostatnim roku", subtitle = paste(product$nazwa_pozycja_2, "\n", "lokalizacja: ", product$jednostka_terytorialna, sep = ""), x = "Okres", y = "Cena"))
+    }
+}
+
+
+
+create_plot_for_every_group(masovian_voievoidship)
+create_plot_for_every_group(groups_pl)
+
+         
